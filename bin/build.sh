05003a72f844bd0ff90cf496f33486f7a34e33be
Pedro Baracho <pedropbaracho@gmail.com>
2024-11-08 20:13:43 -0800

    Enable hot-reloading

--- bin/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-podman build -t formsio-dev -f Dockerfile .
--- bin/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-podman run -it --rm \
-  -p 3000:3000 \
-  -v $(pwd):/app:z \
-  --security-opt label=disable \
-  formsio-dev 
