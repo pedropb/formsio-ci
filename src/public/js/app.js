0ed2363e7fe666884edbcaccedfe9770478b2312
Pedro Baracho <pedropbaracho@gmail.com>
2024-11-08 20:46:17 -0800

    Minor improvements to UI

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,56 +1,54 @@
 Formio.createForm(document.getElementById('formio'), {
-  components: [
-    {
-      type: 'select',
-      key: 'title',
-      label: 'Title',
-      placeholder: 'Select a title',
-      validate: {
-        required: true
-      },
-      data: {
-        values: [
-          { label: 'Mr.', value: 'Mr.' },
-          { label: 'Mrs.', value: 'Mrs.' },
-          { label: 'Ms.', value: 'Ms.' },
-          { label: 'Dr.', value: 'Dr.' },
-          { label: 'Prof.', value: 'Prof.' }
-        ]
-      },
-      input: true
-    },
-    {
-      type: 'textfield',
-      key: 'firstName',
-      label: 'First Name',
-      placeholder: 'Enter your first name',
-      input: true
-    },
-    {
-      type: 'textfield',
-      key: 'lastName',
-      label: 'Last Name',
-      placeholder: 'Enter your last name',
-      validate: {
-        required: true
-      },
-      input: true
-    },
-    {
-      type: 'button',
-      action: 'submit',
-      label: 'Submit',
-      theme: 'primary'
-    }
-  ]
-}).then(function(form) {
-  form.on('submitButton', function(event) {
-    event.preventDefault();
-    
-    const data = form.submission.data;
-    
-    const greeting = document.getElementById('greeting');
-    greeting.innerHTML = `Hello, ${data.title} ${data.firstName || ''} ${data.lastName}`;
-    greeting.classList.remove('hidden');
-  });
+    components: [
+        {
+            type: 'select',
+            key: 'title',
+            label: 'Title',
+            placeholder: 'Select a title',
+            validate: {
+                required: true
+            },
+            data: {
+                values: [
+                    { label: 'Mr.', value: 'Mr.' },
+                    { label: 'Mrs.', value: 'Mrs.' },
+                    { label: 'Ms.', value: 'Ms.' },
+                    { label: 'Dr.', value: 'Dr.' },
+                    { label: 'Prof.', value: 'Prof.' }
+                ]
+            },
+            input: true
+        },
+        {
+            type: 'textfield',
+            key: 'firstName',
+            label: 'First Name',
+            placeholder: 'Enter your first name',
+            input: true
+        },
+        {
+            type: 'textfield',
+            key: 'lastName',
+            label: 'Last Name',
+            placeholder: 'Enter your last name',
+            validate: {
+                required: true
+            },
+            input: true
+        },
+        {
+            type: 'button',
+            action: 'submit',
+            label: 'Submit',
+            theme: 'primary'
+        }
+    ]
+}).then(function (form) {
+    form.on('submit', function (submission) {
+        const data = submission.data;
+
+        const greeting = document.getElementById('greeting');
+        greeting.innerHTML = `Hello, ${data.title} ${data.firstName || ''} ${data.lastName}`;
+        greeting.classList.remove('hidden');
+    });
 }); 
